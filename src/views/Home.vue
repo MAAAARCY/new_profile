40bd4c029b7825a2110a9ebfe6c570b4605297ed
MAAAARCY <masatarou.1003@icloud.com>
2021-08-27 21:58:25 +0900

    Update: home.vue作成

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,12 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/marcy.svg">
+    <img class="home-logo" alt="logo" src="../assets/marcy.svg">
+    <div class="home-profile">
+      <p class="name">Marcy</p>
+      <ul>
+        <li>twitter: @_marcy___</li>
+      </ul>
+    </div>
   </div>
 </template>
 
@@ -11,3 +17,34 @@ export default {
   components: {}
 }
 </script>
+
+<style>
+
+.home {
+  text-align: center;
+}
+
+.home-logo{
+  width:300px;
+  height:300px;
+  object-fit:cover;
+  border-radius:50%;
+}
+
+.home-profile {
+  display: block;
+}
+
+.home-profile ul {
+  list-style: none;
+  display: inline-block;
+}
+
+.home-profile ul li{
+  text-align: left;
+}
+
+.name {
+  font-size: 150%;
+}
+</style>
